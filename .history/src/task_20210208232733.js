3dfe816a0302dbae7d58b4193621deb7608749ae
steelerx155 <steelerx155@gmail.com>
2021-02-08 23:28:30 -0500

    added Task.all = [] to task.js

---
+++ .history/src/task_20210208232733.js
@@ -0,0 +1,13 @@
+class Tasks{
+
+    constructor(){
+        this.id = Task.id
+        this.content = Task.content
+        this.category = Task.category
+        this.date = Task.date
+    }
+
+    renderTaskCard(){
+        
+    }
+}
