21496940a7267218701714d17d830c48b436b72a
steelerx155 <steelerx155@gmail.com>
2021-02-08 23:05:38 -0500

    added DOMContentLoaded and gettodos function to mainjs

---
+++ src/main.js
@@ -0,0 +1,13 @@
+'use strict';
+const backend_url = "http://localhost:3000/tasks"
+debugger
+
+function counter() {
+  let seconds = 0;
+  setInterval(() => {
+    seconds += 1;
+    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
+  }, 1000);
+}
+
+counter();
